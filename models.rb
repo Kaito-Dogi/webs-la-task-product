6d9ae612a4ddbebc22c56dd19a584c685757d642
Kaito-Dogi <kendobu0405@gmail.com>
2021-09-09 14:10:00 +0000

    add: データベースのアソシエーションを定義

--- models.rb
+++ models.rb
@@ -2,3 +2,23 @@ require 'bundler/setup'
 Bundler.require
 
 ActiveRecord::Base.establish_connection
+
+class User < ActiveRecord::Base
+    has_secure_password
+    
+    has_many :posts
+    has_many :favorites
+    has_many :favorite_posts, through: :favorites, source: :posts
+end
+
+class Post < ActiveRecord::Base
+    belongs_to :user
+    
+    has_many :favorites
+    has_many :favorite_users, through: :favorites, source: :users
+end
+
+class Favorite < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :post
+end
