f8c1d0a56969586f1bb89c07867ecab2479a8ad7
Kaito-Dogi <kendobu0405@gmail.com>
2021-09-09 14:52:07 +0000

    add: login機能の追加

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+require 'sinatra/activerecord'
 require './models'
 require 'dotenv/load'
 
+enable :sessions
+
 get '/' do
     erb :index
+end
+
+post '/signin' do
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+        session[:user] = user.id
+        puts session.to_hash
+    end
+    redirect '/'
 end
